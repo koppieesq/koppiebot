4aad4d9d23409ad0a17c9f1e28b2e92058ecb942
Jordan Koplowicz <jordan@koplowicz.com>
2024-05-20 13:18:56 -0700

    Cleanup for Discord bot

--- chat.py
+++ chat.py
@@ -10,43 +10,21 @@ from discord.ext import commands
 df = pd.read_csv('embeddings.csv', index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
-# Define a client for the Discord bot
-class KoppieBot(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+# Define Discord bot
+intents = discord.Intents.default()
+intents.message_content = True
+discord_bot_token = os.environ['DISCORD_BOT_TOKEN']
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
-        
-        answer = answer_question(df, question=message.content, debug=True)
-        await message.channel.send(answer)
+@bot.command()
+async def ask(ctx):
+    # Strip command from question before passing it to AI
+    question = ctx.message.content.replace('/ask ', '')
+    answer = answer_question(df, question=question, debug=False)
+    await ctx.send(answer)
 
-if __name__ == '__main__':
-  print('Got this far')
-  with daemon.DaemonContext():
-    print('Initializing daemon...')
-    # Define Discord bot
-    intents = discord.Intents.default()
-    intents.message_content = True
-    # client = KoppieBot(intents=intents)
-
-    discord_bot_token = os.environ['DISCORD_BOT_TOKEN']
-    # client.run(discord_bot_token)
-
-    bot = commands.Bot(command_prefix='/', intents=intents)
-
-    @bot.command()
-    async def ping(ctx):
-        await ctx.send('pong')
-    
-    @bot.command()
-    async def ask(ctx, question):
-        answer = answer_question(df, question=question, debug=True)
-        await ctx.send(answer)
-
-    bot.run(discord_bot_token)
+bot.run(discord_bot_token)
