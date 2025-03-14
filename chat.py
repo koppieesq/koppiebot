19ab3013bc32bd7f5967ef29092ca75efbb394e1
Jordan Koplowicz <koppiejd@gmail.com>
2025-03-14 08:51:36 -0700

    2nd draft - final + .idea

--- chat.py
+++ chat.py
@@ -8,6 +8,7 @@ from questions import answer_question
 import daemon
 import discord
 from discord.ext import commands
+import time
 
 # Load environment variables from a .env file, if it exists (not in git)
 load_dotenv()
@@ -20,10 +21,26 @@ df = pd.read_csv(f"{current_path}/embeddings.csv", index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 # Define Discord bot
-intents = discord.Intents.default()
-intents.message_content = True
-discord_bot_token = os.environ['DISCORD_BOT_TOKEN']
-bot = commands.Bot(command_prefix='/', intents=intents)
+# intents = discord.Intents.default()
+# intents.message_content = True
+# discord_bot_token = os.environ['DISCORD_BOT_TOKEN']
+# bot = commands.Bot(command_prefix='/', intents=intents)
+
+# Define langchain bot.
+# This is a daemon process that runs in the background.
+with daemon.DaemonContext():
+  api_key = os.environ['LANGSMITH_API_KEY']
+  while True:
+    try:
+      # Load the blog knowledge
+      df = pd.read_csv(f"{current_path}/embeddings.csv", index_col=0)
+      df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+      print("Loaded blog knowledge")
+      break
+    except Exception as e:
+      print(e)
+      print("Retrying in 5 seconds...")
+      time.sleep(5)
 
 @bot.command()
 async def ping(ctx):
