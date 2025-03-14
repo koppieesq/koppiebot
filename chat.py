22a240dda2b4868ad36d8c7cb89c67588c0da752
Jordan Koplowicz <koppiejd@gmail.com>
2025-03-14 10:28:37 -0700

    3rd draft: it works! - locally

--- chat.py
+++ chat.py
@@ -3,18 +3,23 @@
 import getpass
 import os
 from dotenv import load_dotenv
+import bs4
 from langchain.chat_models import init_chat_model
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
-import bs4
 from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langgraph.graph import MessagesState, StateGraph, END
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
 from typing_extensions import List, TypedDict
 import json
 import requests
+from IPython.display import Image, display
 
 # Load environment variables from a .env file, if it exists (not in git)
 load_dotenv()
@@ -27,27 +32,14 @@ llm = init_chat_model("gpt-4o", model_provider="openai")
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 vector_store = InMemoryVectorStore(embeddings)
 
-# Load the blog knowledge
-# df = pd.read_csv(f"{current_path}/embeddings.csv", index_col=0)
-# df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
-
 # Load the environment variable for the blog URL
 blog_url = os.environ.get("API_URL")
-
-# Load the blog knowledge
-# loader = WebBaseLoader(
-#     web_paths=(blog_url,),
-#     bs_kwargs=dict(
-#         parse_only=bs4.SoupStrainer(
-#             class_=("post-content", "post-title", "post-header")
-#         )
-#     ),
-# )
-# docs = loader.load()
 
 # For development environments only
 is_dev = os.getenv('ENVIRONMENT') == 'development'
 verify_ssl = False if is_dev else True
+
+# Fetch the latest blog data.
 response = requests.get(blog_url, verify=verify_ssl)
 
 try:
@@ -56,7 +48,6 @@ except json.JSONDecodeError as e:
     print(f"Error parsing JSON: {e}")
 
 docs = [Document(page_content=post['body'][0]['value']) for post in data]
-# print(docs)
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 all_splits = text_splitter.split_documents(docs)
@@ -76,22 +67,99 @@ class State(TypedDict):
 
 
 # Define application steps
-def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
-    return {"context": retrieved_docs}
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
 
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
 
 
 # Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
+# graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+# graph_builder.add_edge(START, "retrieve")
+# graph = graph_builder.compile()
+graph_builder = StateGraph(MessagesState)
 
-response = graph.invoke({"question": "Who is Jordan Koplowicz?"})
-print(response["answer"])
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+config = {"configurable": {"thread_id": "koppiebot"}}
+
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+# response = graph.invoke({"question": "Who is Jordan Koplowicz?"})
+# print(response["answer"])
+input_message = "Who is Jordan Koplowicz?"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
